bf8a216c2e2cc2714c582efc2314c88b6074af6c
Côme Peco <come@pecorari.fr>
2023-11-30 16:31:27 +0100

    test: action yml test in repo

--- src/index.js
+++ src/index.js
@@ -5,35 +5,40 @@ const analysis = require('../src/analysis.js');
 const webhook = require('../src/discord.js');
 
 async function run() {
-	const payload = github.context.payload;
-	const repository = payload.repository.full_name;
-	const commits = payload.commits;
-	const size = commits.length;
-	const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
+  const payload = github.context.payload;
+  console.log(`Received event payload: ${JSON.stringify(payload, undefined, 2)}`);
+  const repository = payload.repository.full_name;
+  const commits = payload.commits;
+  const size = commits.length;
+  const branch = payload.ref.split('/')[payload.ref.split('/').length - 1];
 
-	console.log(`Received payload ${JSON.stringify(payload, null, 2)}`);
-	console.log(`Received ${commits.length}/${size} commits...`);
+  console.log(`Received ${commits.length}/${size} commits...`);
 
-	if (commits.length === 0) {
-        // This was likely a "--tags" push.
-        console.log(`Aborting analysis, found no commits.`);
-		return;
-	}
+  if (commits.length === 0) {
+    // This was likely a "--tags" push.
+    console.log(`Aborting analysis, found no commits.`);
+    return;
+  }
 
-    const id = core.getInput("id");
-    const token = core.getInput("token");
+  const id = core.getInput('id');
+  const token = core.getInput('token');
 
-	analysis.start(isSkipped(payload.head_commit)).then((report) => {
-        webhook.send(id, token, repository, branch, payload.compare, commits, size, report).catch(err => core.setFailed(err.message));
-    }, err => core.setFailed(err));
+  analysis.start(isSkipped(payload.head_commit)).then(
+    (report) => {
+      webhook
+        .send(id, token, repository, branch, payload.compare, commits, size, report)
+        .catch((err) => core.setFailed(err.message));
+    },
+    (err) => core.setFailed(err)
+  );
 }
 
 try {
-	run();
+  run();
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
 function isSkipped(commit) {
-	return commit.message.toLowerCase().includes("[skip]");
+  return commit.message.toLowerCase().includes('[skip]');
 }
